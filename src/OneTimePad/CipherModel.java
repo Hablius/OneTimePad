4d6ad65d388c9ffd1b40d1d8f876ae57cfd0f66e
blankito1 <emil.blank@students.fhnw.ch>
2024-05-01 16:57:18 +0200

    Konstruktor + Verschlüsselungsmethode

--- src/OneTimePad/CipherModel.java
+++ src/OneTimePad/CipherModel.java
@@ -1,4 +1,37 @@
 package OneTimePad;
 
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CipherModel {
+    private List<Integer> oneTimePad;
+    private int currentPosition = 0;
+
+    //Konstruktor
+    public CipherModel(String padFilePath) throws Exception { //Dateipfad als Parameter
+        oneTimePad = Files.lines(Paths.get(padFilePath)) //lädt Zeilen der Datei als Stream, konvertiert String in ein path Objekt
+                .map(String::trim)                       //Zeile wird von Leerzeichen befreit (Anfang, Ende)
+                .filter(line -> !line.isEmpty()) //sicherstellen, dass keine Leerzeichen vorhanden sind
+                .flatMapToInt(String::chars)//transformiert Zeilen in einen IntStream
+                .boxed() // int werte zu Integer umwandeln
+                .collect(Collectors.toList());  //Integer werte werden in Liste gessammelt
+    }
+
+    // Nachricht verschlüsseln
+    public String encrypt(String message) {
+        StringBuilder encrypted = new StringBuilder();
+        for (char character : message.toCharArray()) {
+            if (currentPosition >= oneTimePad.size()) {
+                throw new IllegalStateException("One-Time Pad ist ausgeschöpft.");
+            }
+            int messageValue = character - 32; //ASCII Bereich 32-126 -> -32 = 0-94
+            int padValue = oneTimePad.get(currentPosition++) - 32;
+            char encryptedChar = (char) (((messageValue + padValue) % 95) + 32); //sicherstellen das Ergebnis im druckbaren Bereich bleibt
+            encrypted.append(encryptedChar); //verschlüsselter char wird an Strinbuilder angehängt
+        }
+        return encrypted.toString();
+    }
 }
