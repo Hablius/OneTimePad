938b4c302784e92046d3424f93e51563c8c848aa
Joel <haefeli.joel@gmail.com>
2024-05-09 14:35:12 +0200

    das Problem mit dem Entschlüsseln wurde behoben, die Nachricht kann aber nur einmal Verschlüsselt und Entschlüsselt werden, muss noch angepasst werden

--- src/OneTimePad/CipherModel.java
+++ src/OneTimePad/CipherModel.java
@@ -37,20 +37,22 @@ public class CipherModel {
 
     // Nachricht entschlüsseln
     public String decrypt(String encryptedMessage) {
-        StringBuilder decrypted = new StringBuilder(); // Ergebnis wird Schrittweise aufgebaut
+        StringBuilder decrypted = new StringBuilder();
+        resetPad(); // Stellt sicher, dass currentPosition auf 0 gesetzt ist
         for (char character : encryptedMessage.toCharArray()) {
             if (currentPosition >= oneTimePad.size()) {
                 throw new IllegalStateException("One-Time Pad ist ausgeschöpft.");
             }
             int encryptedValue = character - 32;
-            int padValue = oneTimePad.get(currentPosition++) - 32;
+            int padValue = oneTimePad.get(currentPosition) - 32;
             char decryptedChar = (char) (((encryptedValue - padValue + 95) % 95) + 32);
             decrypted.append(decryptedChar);
+            currentPosition++;
         }
         return decrypted.toString();
     }
 
     public void resetPad() {
         currentPosition = 0;
-    } // Index auf 0
+    }
 }
